e174c9a599e10805dfd3f54de1b24d464e8f97bd
flovian <flovian.owiti@gmail.com>
2022-03-11 01:36:50 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  *
- * Return: Always 0 (Success)
+ * Return: 1  (success)
  */
+
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
