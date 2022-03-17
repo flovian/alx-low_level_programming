7253ce0050d7ecd566d25d1f8d45f6353d03c8ab
flovian <flovian.owiti@gmail.com>
2022-03-17 16:28:30 +0000

    Function that prints the multples of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that prints the multples of two numbers
+ * @a: the int for the paramaters of the function
+ * @b: the int for the paramaters of the function
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
