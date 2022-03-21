fbae66fe8049301f26afc0ebacb4df038c574bb1
flovian <flovian.owiti@gmail.com>
2022-03-21 20:47:49 +0000

    Convert a string to an integer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,13 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-	size_t i;
+	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
+	while (*(src + i))
 	{
-		dest[i] = src[i];
+		*(dest + i) = *(src + i);
+		i++;
 	}
-	dest[i] = '\0';
+	*(dest + i) = '\0';
 	return (dest);
 }
