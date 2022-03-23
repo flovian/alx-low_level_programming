5d85f0d540e5fea3b80fb0e22a7bc9a251619841
flovian <flovian.owiti@gmail.com>
2022-03-23 19:18:07 +0000

    Function that adds two numbers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *cap_string(char *);
 char *leet(char *);
 void print_number(int n);
 char *rot13(char *);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 #endif /* MAIN_H */
