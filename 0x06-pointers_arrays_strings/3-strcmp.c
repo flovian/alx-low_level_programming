18187fd6b2b7b935977e857f9438795885d488cc
flovian <flovian.owiti@gmail.com>
2022-03-23 15:44:29 +0000

    Function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcmp - Function that compares two strings
+ *@s1: input 1
+ *@s2: input 2
+ *Return: Always 0  (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
