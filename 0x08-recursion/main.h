68d9a586096982b16cfb49b41917b1d3150bbe58
flovian <flovian.owiti@gmail.com>
2022-03-30 15:36:06 +0000

    Returns a palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,8 @@ int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
 int is_divisible(int num, int div);
 int is_prime_number(int n);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
 #endif /* MAIN_h */
