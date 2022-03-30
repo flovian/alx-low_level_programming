ee17bc41cc8e44525f9d9c64e366d76f7918d2a7
flovian <flovian.owiti@gmail.com>
2022-03-30 15:22:05 +0000

    Returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int find_sqrt(int num, int root);
 int _sqrt_recursion(int n);
+int is_divisible(int num, int div);
+int is_prime_number(int n);
 
 #endif /* MAIN_h */
