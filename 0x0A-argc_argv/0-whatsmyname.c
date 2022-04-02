dadb07cc43603dbc8425d74395576a9d40ea1f18
flovian <flovian.owiti@gmail.com>
2022-04-02 01:57:47 +0000

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Prints the number of arguments passed to it.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * main - Prints the number of arguments passed into thre function
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always 0
  */
-int main(int argc, char __attribute__((__unused__)) *argv[])
+
+int main(int argc, char *argv[])
 {
+	(void)argv;
+
 	printf("%d\n", argc - 1);
 
 	return (0);
