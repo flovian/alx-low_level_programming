187fc27819ef95f16d4f373ea0979b36fba31322
flovian <flovian.owiti@gmail.com>
2022-04-07 21:15:32 +0000

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,11 +7,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int main(int argc, char *argv[]);
-int _atoi_digit(char x);
-int _isNumber(char *argv);
-void *_calloc(unsigned int nmemb, unsigned int size);
-void *mul_array(char *a1, int len1, char a2, char *a3, int lena);
-void print_array(char *a, int nb);
-int main(int argc, char *argv[]);
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 #endif /* MAIN_H */
