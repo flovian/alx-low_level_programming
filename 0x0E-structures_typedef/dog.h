5adef682a0c50a24915a99977c2c805e8b120b38
flovian <flovian.owiti@gmail.com>
2022-04-11 18:09:21 +0000

    task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,8 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
+int get_len(int i);
+char *str_cpy(char *dest, char *src);
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
