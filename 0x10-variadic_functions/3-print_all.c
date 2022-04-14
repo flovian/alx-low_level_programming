9022047d4b28284d39b66f13767b292be15c7543
flovian <flovian.owiti@gmail.com>
2022-04-14 14:08:34 +0000

    task 5

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - Prints anything, followed by a new line.
+ * @format: A string of characters representing the argument types.
+ * @...: A variable number of arguments to be printed.
+ *
+ * Description: Any argument not of type char, int, float,
+ *              or char * is ignored.
+ *              If a string argument is NULL, (nil) is printed instead.
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	int n = 0, i = 0;
+	char *sep = ", ";
+	char *str;
+
+	va_start(valist, format);
+
+	while (format && format[i])
+		i++;
+
+	while (format && format[n])
+	{
+		if (n  == (i - 1))
+		{
+			sep = "";
+		}
+		switch (format[n])
+		{
+		case 'c':
+			printf("%c%s", va_arg(valist, int), sep);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(valist, int), sep);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(valist, double), sep);
+			break;
+		case 's':
+			str = va_arg(valist, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s%s", str, sep);
+			break;
+		}
+		n++;
+	}
+	printf("\n");
+	va_end(valist);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,9 +4,21 @@
 /* Description: Header files containing prototypes for all the functions
  *               used in the 0x10 -variadic_functions directory
 */
+
+
 #include <stdarg.h>
+
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *         a data type corresponding to symbol.
+ */
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /*VARIADIC_FUNCTIONS_H */
