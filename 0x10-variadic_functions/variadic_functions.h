b5c352ceb13a1d891e4d23a3ce3890b03f016067
flovian <flovian.owiti@gmail.com>
2022-04-14 13:14:44 +0000

    Initial commit + task 0

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+/* Description: Header files containing prototypes for all the functions
+ *               used in the 0x10 -variadic_functions directory
+*/
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+#endif /*VARIADIC_FUNCTIONS_H */
