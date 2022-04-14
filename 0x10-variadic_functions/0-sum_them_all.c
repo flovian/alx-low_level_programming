919a950ee44a2022cfcf63ea871bebb3576cc8db
flovian <flovian.owiti@gmail.com>
2022-04-14 14:36:43 +0000

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,10 @@
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int i, sum = 0;
+	unsigned int sum = 0, i;
 
 	if (n == 0)
 		return (0);
@@ -21,9 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
-	{
-		sum += va_arg(valist, const unsigned int);
-	}
+		sum += va_arg(valist, int);
 
 	va_end(valist);
 
