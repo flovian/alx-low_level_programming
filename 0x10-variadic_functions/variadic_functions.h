cfcfe93b816e278330fccc36bfcfcbf669ff2a34
flovian <flovian.owiti@gmail.com>
2022-04-14 15:00:17 +0000

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,7 +16,13 @@
  *         a data type corresponding to symbol.
  */
 
-int _putchar(char c);
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
