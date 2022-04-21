bad555875b7bc1a79a8ed929a724d167d0d2f637
flovian <flovian.owiti@gmail.com>
2022-04-21 17:25:23 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,16 @@
  *
  * Return: The number of nodes in h.
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	const list_t *tmp;
+	unsigned int i;
 
-	while (h)
+	tmp = h;
+	for (i = 0; tmp; i++)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
 	}
-
-	return (nodes);
+	return (i);
 }
